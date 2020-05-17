51bb01ba95839b13a13329d1f32b41bb56df280c
javier <javier@dojodevs.io>
2020-05-17 02:59:46 -0500

    Authentication with firebase: login and signup actions implemented. Managing loading state and errors

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,19 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
 import {
   ScrollView,
   View,
   KeyboardAvoidingView,
   StyleSheet,
-  Button
+  Button,
+  ActivityIndicator,
+  Alert
 } from 'react-native';
 
 import { LinearGradient } from 'expo-linear-gradient';
+import { useDispatch } from 'react-redux';
 
 import Input from '../../components/UI/Input';
 import Card from '../../components/UI/Card';
 import Colors from '../../constants/Colors';
+import * as authActions from '../../store/actions/auth';
+
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
 
 const AuthScreen =  props => {
+  const [isSignup, setIsSignUp] = useState(false);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const authHandler = async () => {
+    let action;
+    setError(null);
+    setIsLoading(true);
+    if(isSignup) {
+      action = authActions.signup(formState.inputValues.email, formState.inputValues.password);
+    } else {
+      action = authActions.login(formState.inputValues.email, formState.inputValues.password);
+    }
+    try {
+      await dispatch(action);
+    } catch(err) {
+      setError(err.message)
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if(error) {
+      Alert.alert('An error Ocurred', error, [{text: 'Ok'}])
+    }
+  }, [error]);
+
+  const inputChangeHandler = useCallback(
+    (inputIdentifier, inputValue, inputValidity) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier
+      });
+    },
+    [dispatchFormState]
+  );
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: '',
+      password: ''
+    },
+    inputValidities: {
+      email: false,
+      password: false
+    },
+    formIsValid: false
+  });
+
   return (
     <KeyboardAvoidingView
       behavior="padding"
@@ -30,8 +111,8 @@ const AuthScreen =  props => {
               required
               email
               autoCapitalize="none"
-              errorMessage="Please enter a valid email address"
-              onInputChange={() => {}}
+              errorText="Please enter a valid email address"
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <Input
@@ -42,15 +123,32 @@ const AuthScreen =  props => {
               required
               minLength={5}
               autoCapitalize="none"
-              errorMessage="Please enter a valid password"
-              onInputChange={() => {}}
+              errorText="Please enter a valid password"
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <View style={styles.authButton}>
-              <Button title="Login" color={Colors.primary} onPress={() => {}}/>
+              {
+                isLoading ? (
+                  <ActivityIndicator
+                    size="small"
+                    color={Colors.primary}
+                  />
+                ) : (
+                  <Button
+                    title={isSignup ? 'Signup' : 'Login'}
+                    color={Colors.primary}
+                    onPress={authHandler}
+                  />
+                )
+              }
             </View>
             <View style={styles.authButton}>
-              <Button title="Switch to Sign up" color={Colors.accent} onPress={() => {}}/>
+              <Button
+                title={`Switch to ${isSignup ? 'Login' : 'Sign Up'}`}
+                color={Colors.accent}
+                onPress={() =>setIsSignUp(prevState => !prevState)}
+              />
             </View>
           </ScrollView>
         </Card>
---
+++ store/actions/auth.js
@@ -0,0 +1,60 @@
+export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
+
+export const signup = (email, password) => {
+  return async dispatch => {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBU7uJNDl3C2_1iZevr7J9KLNXy5bVBhck', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      })
+    });
+
+    if(!response.ok) {
+      const errorResData = await response.json();
+      console.log(errorResData);
+      throw new Error(errorResData.error.message);
+    }
+
+    const resData = await response.json();
+    console.log(resData)
+
+    dispatch({
+      type: SIGNUP
+    });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBU7uJNDl3C2_1iZevr7J9KLNXy5bVBhck', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      })
+    });
+
+    if(!response.ok) {
+      const errorResData = await response.json();
+      console.log(errorResData);
+      throw new Error(errorResData.error.message);
+    }
+
+    const resData = await response.json();
+    console.log(resData)
+
+    dispatch({
+      type: LOGIN
+    });
+  };
+};
